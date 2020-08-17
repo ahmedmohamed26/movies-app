85a92a408c19eb45854439304031d553bfb21440
ahmed mohamed <ahmedmohamed26.github.io>
2020-08-18 01:09:48 +0200

    handle env file

--- src/services/movies.jsx
+++ src/services/movies.jsx
@@ -1,26 +1,28 @@
 import axios from 'axios';
-import API_K from '../config_keys';
 
 const URL_API = 'https://api.themoviedb.org/3/';
+const API_KEY =`${process.env.REACT_APP_API_KEY}`
 export const URL_IMAGE = 'https://image.tmdb.org/t/p/w500/';
 
+
+
 export async function NowPlayingMovies(typeMovies,numberPage) {
-    let response = await axios.get(`${URL_API}movie/${typeMovies}?api_key=${API_K}&language=en-US&page=${numberPage}`)
+    let response = await axios.get(`${URL_API}movie/${typeMovies}?api_key=${API_KEY}&language=en-US&page=${numberPage}`)
     return response;
 }
 
 export async function movieDetails(idMovie) {
-    let response = await axios.get(`${URL_API}movie/${idMovie}?api_key=${API_K}&language=en-US`)
+    let response = await axios.get(`${URL_API}movie/${idMovie}?api_key=${API_KEY}&language=en-US`)
     return response;
 }
 
 export async function searchMovies(keyword) {
-    let response = await axios.get(`${URL_API}search/movie?api_key=${API_K}&language=en-US&query=${keyword}&page=1&include_adult=false`)
+    let response = await axios.get(`${URL_API}search/movie?api_key=${API_KEY}&language=en-US&query=${keyword}&page=1&include_adult=false`)
     return response;
 }
 
 export async function videoMovieId(Id) {
-    let response = await axios.get(`${URL_API}movie/${Id}/videos?api_key=${API_K}&language=en-US`)
+    let response = await axios.get(`${URL_API}movie/${Id}/videos?api_key=${API_KEY}&language=en-US`)
     return response;
 }
 
