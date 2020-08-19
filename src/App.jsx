e1bdaa30ee8dc356d25f3ea887f21d9327708eba
ahmed mohamed <ahmedmohamed26.github.io>
2020-08-19 12:08:37 +0200

    fix redirect page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Header from './shared/header/header';
 import Footer from './shared/footer/footer';
 import Home from './pages/home/home';
@@ -11,7 +11,6 @@ function App() {
 		<BrowserRouter>
 			<div className='App'>
 				<Header />
-				<Redirect path='/' component={Home} />
 				<Route exact path='/' component={Home} />
 				<Route exact path='/movies/:type' component={Category} />
 				<Route exact path='/movie-details/:id' component={MovieDetails} />
